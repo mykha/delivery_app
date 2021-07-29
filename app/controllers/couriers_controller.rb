d83c952dd5658cb0a3c3ba7584ce386b54f2fd40
mykha <efimenko.mykhailo@gmail.com>
2021-07-29 23:31:18 +0300

    Create, Edit, Update couriers

--- app/controllers/couriers_controller.rb
+++ app/controllers/couriers_controller.rb
@@ -1,4 +1,43 @@
 class CouriersController < ApplicationController
   def index
+    @couriers = Courier.all
   end
+
+  def show
+    @courier = Courier.find(params[:id])
+  end
+
+  def new
+    @courier = Courier.new
+  end
+
+  def create
+    @courier = Courier.new(courier_params)
+
+    if @courier.save
+      redirect_to @courier
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @courier = Courier.find(params[:id])
+  end
+
+  def update
+    @courier = Courier.find(params[:id])
+
+    if @courier.update(courier_params_params)
+      redirect_to @courier
+    else
+      render :edit
+    end
+  end
+
+  private
+  def courier_params
+    params.require(:courier).permit(:name, :email)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root "couriers#index"
-  get "/couriers", to: "couriers#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :couriers
 end
